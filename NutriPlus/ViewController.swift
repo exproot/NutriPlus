f9957a80faffc1a48e1bf51ffcd5d2ed8d709ddd
Ertan <ertanyagmur@outlook.com>
2024-10-21 10:18:56 +0300

    initial commit

---
+++ NutriPlus/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NutriPlus
+//
+//  Created by Ertan Yağmur on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
